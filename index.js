c5234cb87755363956c548dc7fe65ba1715448c9
day-thebluehour <day@thebluehour.co.kr>
2024-07-13 17:12:47 +0900

    cors setting

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import {Server} from 'socket.io';
 
-const io = new Server();
+const app = express();
+app.use(cors()); // CORS 설정 추가
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
 io.on('connection', socket => {
-  console.log(`connect: ${socket.id}`, socket.request.headers);
+  console.log('a user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id);
+  });
 
   socket.on('send_message', data => {
-    console.log(123123, data);
-
-    io.emit('receive_message', data);
-  });
-  socket.on('disconnect', () => {
-    console.log(`disconnect: ${socket.id}`);
+    const {message, targetSocketId} = data;
+    io.to(targetSocketId).emit('receive_message', message);
   });
 });
 
-io.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
